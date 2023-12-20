1c9be444baf2077007aaf3e3c28cc7d74c4701c0
makise <10175101106@stu.ecnu.edu.cn>
2023-12-21 00:30:37 +0800

    ops: containerize front end

--- src/global/network.js
+++ src/global/network.js
@@ -1,9 +1,9 @@
 
 
 const BASE_URL_DEBUG = 'http://127.0.0.1:8080';
-const BASE_URL_PROD = 'https://api.recordmk.xyz';
+const BASE_URL_PROD = 'http://backend:8080';
 const BASE_URL_WS_DEBUG = 'ws://127.0.0.1:8080/ws';
-const BASE_URL_WS_PROD = 'wss://api.recordmk.xyz/ws';
+const BASE_URL_WS_PROD = 'ws://backend:8080/ws';
 export const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEBUG;
 export const BASE_URL_WS = process.env.NODE_ENV === 'production' ? BASE_URL_WS_PROD : BASE_URL_WS_DEBUG;
 export const BANGUMI_BASE_URL = 'https://api.bgm.tv';
